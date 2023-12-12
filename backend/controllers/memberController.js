d41eb922fc2cc82999425dcbd5ea9d14f8b6c680
gzuppa <gzuppa86@gmail.com>
2023-12-11 18:29:52 -0600

    feat: members routing

---
+++ backend/controllers/memberController.js
@@ -0,0 +1,32 @@
+const getMembers = async (req, res) => {
+
+}
+
+const newMember = async (req, res) => {
+
+}
+
+const getMember = async (req, res) => {
+
+}
+
+const editMember = async (req, res) => {
+
+}
+
+const deleteMember = async (req, res) => {
+
+}
+
+const addMember = async (req, res) => {
+
+}
+
+export {
+    addMember,
+    deleteMember,
+    editMember,
+    getMember,
+    getMembers,
+    newMember,
+  }
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import userRoutes from './routes/userRoutes.js'
+import memberRoutes from './routes/memberRoutes.js'
 
 const app = express()
 app.use(express.json())
@@ -11,6 +12,7 @@ connectDB()
 
 //Routing
 app.use('/api/users', userRoutes)
+app.use('/api/members', memberRoutes)
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
