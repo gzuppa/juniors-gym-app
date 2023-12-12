22bf24b8ef9670bf81484c8aa90c5a10d786f899
gzuppa <gzuppa86@gmail.com>
2023-12-11 19:31:05 -0600

    feat: update members

--- backend/controllers/memberController.js
+++ backend/controllers/memberController.js
@@ -20,9 +20,38 @@ const newMember = async (req, res) => {
   }
 }
 
-const getMember = async (req, res) => {}
+const getMember = async (req, res) => {
+  const { id } = req.params
+  const member = await Member.findById(id)
 
-const editMember = async (req, res) => {}
+  if(!member) {
+    const error = new Error('Cliente no encontrado')
+    return res.status(404).json({ msg: error.message })
+  }
+  res.json(member)
+}
+
+const editMember = async (req, res) => {
+  const { id } = req.params
+  const member = await Member.findById(id)
+
+  if(!member) {
+    const error = new Error('Cliente no encontrado')
+    return res.status(404).json({ msg: error.message })
+  }
+  member.name = req.body.name || member.name
+  member.payDate = req.body.payDate || member.payDate
+  member.payAmount = req.body.payAmount || member.payAmount
+  member.phone = req.body.phone || member.phone
+  member.age = req.body.age || member.age
+
+  try {
+    const updatedMember = await member.save()
+    res.json(updatedMember)
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 const deleteMember = async (req, res) => {}
 
