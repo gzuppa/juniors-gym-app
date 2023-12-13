8c38b6492d0549e738503709a7b3b1896dcd25bb
gzuppa <gzuppa86@gmail.com>
2023-12-12 20:00:11 -0600

    feat: navigation login bottom

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,15 @@
+import { Link } from 'react-router-dom'
 import JuniorsLogo from '../assets/images/juniors-gym-logo.png'
 
 const Login = () => {
   return (
     <>
-      <div className='flex justify-center items-center'>
-        <img src={JuniorsLogo} alt="JuniorsLogo" className='h-32 mr-2'/>
+      <div className="flex justify-center items-center">
+        <img src={JuniorsLogo} alt="JuniorsLogo" className="h-32 mr-2" />
         <h1 className="text-purple-800 font-black text-4xl font-raleway">
-        Inicia sesión para
-        <span className="text-yellow-300"> administrar usuarios</span>
-      </h1>
+          Inicia sesión para
+          <span className="text-yellow-300"> administrar usuarios</span>
+        </h1>
       </div>
       <form className="my-10 bg-white shadow rounded-lg p-10">
         <div className="my-5">
@@ -45,6 +46,23 @@ const Login = () => {
           className="mb-5 bg-purple-800 text-yellow-300 w-full py-3 uppercase font-bold rounded-xl hover:cursor-pointer hover:bg-yellow-300 hover:text-purple-800 transition-colors"
         />
       </form>
+
+      <nav className="lg:flex lg:justify-between">
+        <Link
+          to="register"
+          className="block text-center my-3 uppercase text-sm font-raleway text-yellow-300"
+        >
+          {' '}
+          Registrar cuenta
+        </Link>
+        <Link
+          to="forgot-password"
+          className="block text-center my-3 uppercase text-sm font-raleway text-yellow-300"
+        >
+          {' '}
+          Recuperar password
+        </Link>
+      </nav>
     </>
   )
 }
