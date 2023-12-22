94b81aa8d7c4524acffa2930be4e740d60b92c84
gzuppa <gzuppa86@gmail.com>
2023-12-21 20:43:39 -0600

    feat: add training model

---
+++ backend/models/Training.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+
+const trainingSchema = mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  status: {
+    type: Boolean,
+    default: false
+  },
+  startDate: {
+    type: Date,
+    required: true,
+    default: Date.now()
+  },
+  level: {
+    type: String,
+    required: true,
+    enum: ['Principiante', 'Intermedio', 'Avanzado', 'Alto Nivel']
+  },
+  member: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Member'
+  }
+}, {
+  timestamps: true
+})
+
+const Training = mongoose.model('Training', trainingSchema)
+export default Training
